0d7d9afa2e7135e0096150bc364b0de8b7a18986
RemiEGUCHI <110374856+03Jr113@users.noreply.github.com>
2022-08-15 09:50:14 +0000

    [Add]book index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
   root to: 'home#top'
   get 'home/about'
+  
+  devise_for :users
+  
+  resources :books, only: [:index, :show, :edit]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
