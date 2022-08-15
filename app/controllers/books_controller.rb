4b3c1722c33fb6081ebbc10d3534467528400ae2
RemiEGUCHI <110374856+03Jr113@users.noreply.github.com>
2022-08-15 12:03:09 +0000

    [Add]error message

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,12 @@
 class BooksController < ApplicationController
-  
+
   def index
-    @book = Book.new
     @books = Book.all
+    @book = Book.new
   end
-  
+
   def create
+    @books = Book.all
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
@@ -20,11 +21,11 @@ class BooksController < ApplicationController
 
   def edit
   end
-  
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
