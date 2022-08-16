5a42996d435f6ed262d5d630073e2e7128d7710f
RemiEGUCHI <110374856+03Jr113@users.noreply.github.com>
2022-08-16 07:36:57 +0000

    [Add]error message

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  
+  before_action :correct_user, only: [:edit, :update]
 
   def index
     @books = Book.all
@@ -29,8 +31,11 @@ class BooksController < ApplicationController
   
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      redirect_to book_path(book.id)
+    else
+      render :edit
+    end
   end
   
   def destroy
@@ -43,6 +48,12 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+  
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to books_path unless @user == current_user
   end
 
 end
