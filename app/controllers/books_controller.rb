25b74fb7137c0ad519c1ff5b8b464b157cc30938
RemiEGUCHI <110374856+03Jr113@users.noreply.github.com>
2022-08-19 06:44:55 +0000

    [Add]notice

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,6 +14,7 @@ class BooksController < ApplicationController
     @post_book = Book.new(book_params)
     @post_book.user_id = current_user.id
     if @post_book.save
+      flash[:notice] = "You have created book successfully."
       redirect_to book_path(@post_book.id)
     else
       render :index
@@ -32,6 +33,7 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     if book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
       redirect_to book_path(book.id)
     else
       render :edit
